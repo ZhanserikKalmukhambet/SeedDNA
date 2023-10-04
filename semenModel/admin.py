39a8ad371517a7cb212f88129a1d9c3c69e3ea37
Zhanserik <zhanserik.kalmukhambet@gmail.com>
2023-10-04 22:11:08 +0600

    Created ImageData with ImageField, condifgured media_root to store input images

--- semenModel/admin.py
+++ semenModel/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from semenModel.models import ImageData
+
 # Register your models here.
+
+admin.site.register(ImageData)
--- semenModel/models.py
+++ semenModel/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+
+class ImageData(models.Model):
+    image = models.ImageField(null=True)
+    percent = models.CharField(max_length=255)
+    fragments = models.IntegerField()
+    fragmented_degrades = models.IntegerField()
+    normals = models.IntegerField()
+
+    class Meta:
+        verbose_name = 'Image Data'
+        verbose_name_plural = 'Image Data'
+
+    def __str__(self):
+        return str(self.pk)
