a4b3add8c5a122563598dddc4870003a117d005d
Zhanserik <zhanserik.kalmukhambet@gmail.com>
2023-10-09 19:36:11 +0600

    Trying to implement make_baw()

--- semenModel/views.py
+++ semenModel/views.py
@@ -1,10 +1,13 @@
 import json
+import os
 
+from PIL import Image, ImageEnhance
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
 from rest_framework import status
 
+from spermDNA.settings import BASE_DIR
 from .serializers import ImageDataSerializer
 
 
@@ -21,6 +24,8 @@ def predict_view(request):
                 print("Error saving data", str(e))
 
             # there i need to call function which makes image baw - make_baw
+            # make_baw()
+
             return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
 
         print("Request Data:", request.data)
@@ -29,5 +34,23 @@ def predict_view(request):
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+# testing : add constants : INPUT_PATH, media_root = os.path.join(BASE_DIR, 'media/'). etc.
 def make_baw():
-    pass
+    media_root = os.path.join(BASE_DIR, 'media/')
+
+    input_path = media_root + 'input_images\Снимок_экрана_2023-10-06_215218.png'
+
+    input_path = os.getcwd() + input_path
+    output_path = media_root + 'baw_images/ready.png'
+
+    img = Image.open(input_path)
+    contrast = ImageEnhance.Contrast(img)
+    r = contrast.enhance(1.5)
+    thresh = 200
+
+    def fn(x): return 255 if x > thresh else 0
+
+    r = r.convert('1').point(fn, mode='1')
+    r.save(fp=output_path)
+    r.close()
+    img.close()
